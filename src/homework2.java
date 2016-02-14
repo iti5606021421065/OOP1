c9e75d9e667809cbe65e9df783fcd5f31eb73695
iti5606021421065 <5606021421065@fitm.kmutnb.ac.th>
2016-02-14 23:49:51 +0700

    การบ้านครั้งที่1
    แก้ไขเป็น Method กับ JOptionPane

--- src/homework2.java
+++ src/homework2.java
@@ -1,67 +1,83 @@
-import java.util.*;
-public class homework2 {
-//2 Trik
+import java.io.*;
+import javax.swing.JOptionPane;
+public class Trik 
+//Trik
+{
+	boolean num1= true,num2=false,num3=false,b=false;
 	
-	public static void main(String[] args) 
+	public void getData(String round)
 	{
-		Scanner scan = new Scanner(System.in);
-		String round;
-		boolean num1= true,num2 = false,num3=false,num4=false;
-		
-		System.out.printf("INPUT A,B,C  = ");
-		round = scan.next();
-		
 		for(int i=0;i<round.length();i++)
 		{
 			char a;
 			a = round.charAt(i);
 			if((a != 'A') && (a != 'B') && (a != 'C'))
 			{
-				System.out.println("Error Character");
+				JOptionPane.showMessageDialog(null,"Error","Message",JOptionPane.ERROR_MESSAGE);
 				round = " ";
 			}
 		}
-		
+	}
+	
+	public void Calc(String round)
+	{
 		for(int i=0;i<round.length();i++)
 		{
 			char a;
 			a = round.charAt(i);
 			if(a == 'A')
 			{
-				num4 = num2;
+				b = num2;
 				num2 = num1;
-				num1 = num4;
+				num1 = b;
 			}
 			else if(a == 'B')
 			{
-				num4 = num3;
+				b = num3;
 				num3 = num2;
-				num2 = num4;
+				num2 = b;
 			}
 			else if(a == 'C')
 			{
-				num4 = num3;
+				b = num3;
 				num3 = num1;
-				num1 = num4;
+				num1 = b;
 			}
 		}
-		
+	}
+	
+	public void Print(String round)
+	{
+
 		if(round.equals(" "))
 		{
 			
 		}
 		else if(num1)
 		{
-			System.out.println("1");
+			JOptionPane.showMessageDialog(null,"1","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
 		else if(num2)
 		{
-			System.out.println("2");
+			JOptionPane.showMessageDialog(null,"2","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
 		else if(num3)
 		{
-			System.out.println("3");
+			JOptionPane.showMessageDialog(null,"3","Message",JOptionPane.INFORMATION_MESSAGE);
 		}
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		String round;
+		
+		round = JOptionPane.showInputDialog(null,"Enter Input : ","Input",JOptionPane.QUESTION_MESSAGE);
+		
+		Trik TK = new Trik();
+		TK.getData(round);
+		TK.Calc(round);
+		TK.Print(round);
 	}
 
 }
