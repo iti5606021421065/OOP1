5ecd33a6fed70b835b9eaed155c2a171ec1998f5
iti5606021421065 <5606021421065@fitm.kmutnb.ac.th>
2016-02-17 13:51:03 +0700

    Update homework_2.java

--- OOP2/homework_2.java
+++ OOP2/homework_2.java
@@ -1,6 +1,19 @@
-
+import javax.swing.JOptionPane;
 public class homework_2 {
 //รับชื่อนามสกุลแล้วแสดงสระที่มีในชื่อนามสกุล A E I O U
+
+static int A=0;
+static int E=0;
+static int I=0;
+static int O=0;
+static int U=0;
+	public String input()
+	{
+		String name = JOptionPane.showInputDialog(null,"Input Name and Surname: = ","INPUT",JOptionPane.QUESTION_MESSAGE);
+		name = name.toUpperCase();
+		return name;
+	}
+	
 	public static void main(String[] args) {
 		
 
