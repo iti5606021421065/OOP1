04f5c9325177a96a07b99911be09833e71a7940f
iti5606021421065 <5606021421065@fitm.kmutnb.ac.th>
2016-02-17 13:49:26 +0700

    Update homework_1.java

--- OOP2/homework_1.java
+++ OOP2/homework_1.java
@@ -1,6 +1,14 @@
-
+import javax.swing.JOptionPane;
 public class homework_1 {
 //รับเวลาเป็นวินาที แล้วแสดงออกเป็น วัน/ชั่วโมง/นาที/วินาที
+
+int sec=0,today=0,hour=0,minute=0;
+public void input()
+	{
+		String ss;
+		ss = JOptionPane.showInputDialog(null,"Input Second : = ","Input",JOptionPane.QUESTION_MESSAGE);
+		sec = Integer.parseInt(ss);
+	}
 	public static void main(String[] args) {
 		
 
