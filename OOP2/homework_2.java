f7ef26ce463e394454676051978589afa4dbc34d
iti5606021421065 <5606021421065@fitm.kmutnb.ac.th>
2016-02-19 00:52:40 +0700

    Update homework_2.java

--- OOP2/homework_2.java
+++ OOP2/homework_2.java
@@ -13,7 +13,25 @@ static int U=0;
 		name = name.toUpperCase();
 		return name;
 	}
-	
+	public void calculate(String name){
+		for (int i=0; i< name.length();i++){
+			if (name.charAt(i) == 'A'){
+				A = A+1;
+			}
+			else if (name.charAt(i) == 'E'){
+				E = E+1;
+			}
+			else if (name.charAt(i) == 'I'){
+				I = I+1;
+			}
+			else if (name.charAt(i) == 'O'){
+				O = O+1;
+			}
+			else if (name.charAt(i) == 'U'){
+				U = U+1;
+			}
+		}
+	}
 	public static void main(String[] args) {
 		
 
